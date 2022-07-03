b12a4e504653013d4f7e07f5aeafea3527a12b52
Hooshyar <hooseyr@gmail.com>
2022-07-03 17:14:57 +0300

    wait for five seconds

---
+++ android/app/src/main/kotlin/app/datacode/futures/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.datacode.futures
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:futures/src/screens/futures_screen_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FuturesScreenView(),
+    );
+  }
+}
